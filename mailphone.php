2855ce7356544b15efb756b461048440170ae0f7
Skmm6 <egor.qaq@gmail.com>
2021-04-27 21:22:12 +0300

    commitFull1.1predrell

---
+++ mailphone.php
@@ -0,0 +1,40 @@
+<?php 
+
+require_once('./phpmailer/PHPMailerAutoload.php');
+$mail = new PHPMailer;
+$mail->CharSet = 'utf-8';
+
+$phone = $_POST['phone'];
+$email = $_POST['email'];
+// $email = $_POST['email'];
+
+//$mail->SMTPDebug = 3;                               // Enable verbose debug output
+
+$mail->isSMTP();                                      // Set mailer to use SMTP
+$mail->Host = 'ssl://smtp.gmail.com';																							// Specify main and backup SMTP servers
+$mail->SMTPAuth = true;                               // Enable SMTP authentication
+$mail->Username = 'egor.qaq@gmail.com'; // Ваш логин от почты с которой будут отправляться письма
+$mail->Password = 'qaqazaqaq1234'; // Ваш пароль от почты с которой будут отправляться письма
+$mail->SMTPSecure = 'ssl';                            // Enable TLS encryption, `ssl` also accepted
+$mail->Port = 465; // TCP port to connect to / этот порт может отличаться у других провайдеров
+
+$mail->setFrom('egor.qaq@gmail.com'); // от кого будет уходить письмо?
+$mail->addAddress('victoriasherbach049@gmail.com');     // Кому будет уходить письмо 
+//$mail->addAddress('ellen@example.com');               // Name is optional
+//$mail->addReplyTo('info@example.com', 'Information');
+//$mail->addCC('cc@example.com');
+//$mail->addBCC('bcc@example.com');
+//$mail->addAttachment('/var/tmp/file.tar.gz');         // Add attachments
+//$mail->addAttachment('/tmp/image.jpg', 'new.jpg');    // Optional name
+$mail->isHTML(true);                                  // Set email format to HTML
+
+$mail->Subject = 'Заявка с сайта Софи';
+$mail->Body    = '' . 'Клиент с почтой '  . '' .$email . ' оставил заявку, его телефон ' .$phone; //. '<br>Почта этого пользователя: ' .$email;
+$mail->AltBody = '';
+
+if (!$mail->send()) {
+    echo 'error';
+} else  {
+    header('location: thank-you.html');
+}
+?>
